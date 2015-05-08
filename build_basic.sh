b3f7c87eb4fb9f0503f1cd68a9e4cb2b63708e44
Matthew P. Grosvenor <mgrosvenor@gmail.com>
2015-05-08 04:42:18 +0100

    got compling on linux again

--- build_basic.sh
+++ build_basic.sh
@@ -4,10 +4,10 @@ set -o nounset
 set -o errexit
 
 DEFINES=""
-#CFLAGS="-g -Ideps -Ideps -D_XOPEN_SOURCE=700 -D_BSD_SOURCE -std=c11 -Werror -Wall -Wextra -pedantic -Wno-missing-field-initializers"
-CFLAGS="-g -Ideps -Ideps -std=c11 -Werror -Wall -Wextra -pedantic -Wno-missing-field-initializers"
-LINKFLAGS="-g -Ldeps/chaste "
-#LINKFLAGS="-g -Ldeps/chaste -lrt"
+CFLAGS="-g -Ideps -Ideps -D_XOPEN_SOURCE=700 -D_BSD_SOURCE -std=c11 -Werror -Wall -Wextra -pedantic -Wno-missing-field-initializers"
+#CFLAGS="-g -Ideps -Ideps -std=c11 -Werror -Wall -Wextra -pedantic -Wno-missing-field-initializers"
+#LINKFLAGS="-g -Ldeps/chaste "
+LINKFLAGS="-g -Ldeps/chaste -lrt"
 CAKECONFIG=cake.conf
 
 SRC=src/fast_net.c
